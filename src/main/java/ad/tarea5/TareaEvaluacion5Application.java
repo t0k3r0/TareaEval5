d7bc92bebec9829259dff569afeb0e2b9bbe9399
Joseba Sanchez <josebsanchez@birt.eus>
2022-02-27 13:46:12 +0100

    Initial Commit

---
+++ src/main/java/ad/tarea5/TareaEvaluacion5Application.java
@@ -0,0 +1,13 @@
+package ad.tarea5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaEvaluacion5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaEvaluacion5Application.class, args);
+	}
+
+}
